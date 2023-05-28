ac2b98693686b14141dd093b049585387ec9963c
lluwa3 <lucasluwa@yahoo.com>
2023-05-28 18:40:23 -0400

    PE/DivYield Processing Completed

--- DataProcessor.py
+++ DataProcessor.py
@@ -24,7 +24,7 @@ def main():
     excelWriter()
 
 def excelWriter():
-    global currSheet, yearsTTM
+    global currSheet, yearsTTM, row
     stoppingIndecies = [702, 1220, 1053, 73, 45, 148, 175, 792, 261, 182, 1498, 68, 680]
     counter = 0;
     for row in rawDataBook['Miscellaneous'].iter_rows(15, 17):
@@ -32,15 +32,18 @@ def excelWriter():
         tempWKST = core1['Miscellaneous']
         sheetIndex = sheetNames.index('Miscellaneous')
         #Check 1 and 3 first
-        if redFlagsS1(row, currSheet) and redFlagsS3(row, currSheet):
+        if redFlagsS1(currSheet) and redFlagsS3(currSheet):
             continueRunning = True
             #SERIES 2 - More processing than 1 and 3. We don't wanna perform this twice. Errors handled inside here.
                 #s2Indecies = [18, 17, 19, 20, 25, 27, 28, 29, 30, 31, 33]
-            yearsTTM, continueRunning = yearProcessing(row, 18)
-            if continueRunning: high, low, continueRunning = highLowValueProcessing(row, 17)
-            if continueRunning: revenuePerShare, continueRunning = rpsProcessing(row, 19)
-            if continueRunning: earningsPerShare, continueRunning = epsProcessing(row, 20)
-            if continueRunning: 
+            yearsTTM, continueRunning = yearProcessing(18)
+            if continueRunning: highSeries, lowSeries, continueRunning = series2Processor(17, 15, 1, ['.'])
+            if continueRunning: revenuePerShare, continueRunning = series2Processor(19, 16, 2, ['.', '{', '}'])
+            if continueRunning: earningsPerShare, continueRunning = series2Processor(20, 17, 3, ['.', '(', ')', '{', '}'])
+            if continueRunning: priceEarnings, continueRunning = series2Processor(25, 18, 4, ['.', '-', ' '])
+            if continueRunning: divYield, continueRunning = series2Processor(27, 19, 5, ['.', '-', ' '])
+            # if continueRunning: 
+
             if continueRunning: 
                 #TAGS
                 for i in range(1,6):
@@ -65,64 +68,57 @@ def excelWriter():
                     counter += 1
                 rowIndecies[sheetIndex] += 1
                 if generateSheetToggle: core1.save("ProcessedSheets\\" + monthYR + "\\" + writeExcelFileName)
-# if avg all roic or last 5 avg or last 3 above 2 we good. otherwise eliminated
 
-def epsProcessing(row, rowIndex):
-    errorNum = 17
-    earnings = [-1] * len(yearsTTM)
-    if row[rowIndex].value[0] == ')': row[rowIndex].value = row[rowIndex].value[1:] # Temporary until infinity issue is fixed
-    if row[rowIndex].value == None: return earnings, errorHandler(row, errorNum, currSheet)
+# if avg all roic or last 5 avg or last 3 above 2 we good. otherwise eliminated
+def series2Processor(rowIndex, errorNum, idNum, additionalSet):
+    if row[rowIndex].value == None: return None, errorHandler(errorNum, currSheet)
+    if row[rowIndex].value[0] == ')' and idNum == 3: row[rowIndex].value = row[rowIndex].value[1:] # Temporary until infinity issue is fixed
+    output = [-1] * len(yearsTTM)
+    if idNum == 1: output2 = [-1] * len(yearsTTM)
     individualValues = list()
-    additionalSet = ['.', '(', ')']
     rawNumbers = removeNonNumeric(row[rowIndex].value, additionalSet) 
-    if len(rawNumbers) == 0: return earnings, errorHandler(row, errorNum, currSheet)
+    if len(rawNumbers) == 0: return None, errorHandler(errorNum, currSheet)
     while len(rawNumbers) > 0:
+        individualValues, rawNumbers = series2ProcessorCondHelper(idNum, individualValues, rawNumbers)
+    for i in range(0, len(yearsTTM)):
+        if yearsTTM[i] == 1 and len(individualValues) > 0:
+            output[i] = individualValues.pop(len(individualValues) - 1)
+            if idNum == 1: output2[i] = individualValues.pop(len(individualValues) - 1)
+    if idNum == 1: return output, output2, True
+    return output, True
+
+def series2ProcessorCondHelper(idNum, individualValues, rawNumbers):
+    if idNum in [4]:
         if rawNumbers[0] == '(':
             individualValues.append(rawNumbers[0:rawNumbers.index(')') + 1])
             rawNumbers = rawNumbers[rawNumbers.index(')') + 1:]
+    if idNum in [4, 5]:
+        if rawNumbers[0] == '-':
+            individualValues.append(str(0.0))
+            rawNumbers = rawNumbers[3:]
+        else:
+            individualValues.append(rawNumbers[0:rawNumbers.index('.') + 2])
+            rawNumbers = rawNumbers[rawNumbers.index('.') + 2:]
+    if idNum in [3]:
+        if rawNumbers[0] == '(':
+            individualValues.append(rawNumbers[0:rawNumbers.index(')') + 1])
+            rawNumbers = rawNumbers[rawNumbers.index(')') + 1:]
+        elif rawNumbers[0] == '{':
+            individualValues.append(rawNumbers[0:rawNumbers.index('}') + 1])
+            rawNumbers = rawNumbers[rawNumbers.index('}') + 1:]
         else:
             individualValues.append(rawNumbers[0:rawNumbers.index('.') + 3])
             rawNumbers = rawNumbers[rawNumbers.index('.') + 3:]
-    for i in range(0, len(yearsTTM)):
-        if yearsTTM[i] == 1 and len(individualValues) > 0:
-            earnings[i] = individualValues.pop(len(individualValues) - 1)
-    return earnings, True
-
-def rpsProcessing(row, rowIndex):
-    errorNum = 16
-    revenue = [-1] * len(yearsTTM)
-    if row[rowIndex].value == None: return revenue, errorHandler(row, errorNum, currSheet)
-    individualValues = list()
-    additionalSet = ['.']
-    rawNumbers = removeNonNumeric(row[rowIndex].value, additionalSet) 
-    if len(rawNumbers) == 0: return revenue, errorHandler(row, errorNum, currSheet)
-    while len(rawNumbers) > 0:
+    if idNum in [2]:
+        if rawNumbers[0] == '{':
+            individualValues.append(rawNumbers[0:rawNumbers.index('}') + 1])
+            rawNumbers = rawNumbers[rawNumbers.index('}') + 1:]
+    if idNum in [1, 2]:
         individualValues.append(rawNumbers[0:rawNumbers.index('.') + 3])
         rawNumbers = rawNumbers[rawNumbers.index('.') + 3:]
-    for i in range(0, len(yearsTTM)):
-        if yearsTTM[i] == 1 and len(individualValues) > 0:
-            revenue[i] = individualValues.pop(len(individualValues) - 1)
-    return revenue, True
+    return individualValues, rawNumbers
 
-def highLowValueProcessing(row, rowIndex):
-    errorNum = 15
-    high = [-1] * len(yearsTTM)
-    low = [-1] * len(yearsTTM)
-    if row[rowIndex].value == None: return high, low, errorHandler(row, errorNum, currSheet)
-    individualValues = list()
-    additionalSet = ['.']
-    rawNumbers = removeNonNumeric(row[rowIndex].value, additionalSet) 
-    if len(rawNumbers) == 0: return high, low, errorHandler(row, errorNum, currSheet)
-    while len(rawNumbers) > 0:
-        individualValues.append(rawNumbers[0:rawNumbers.index('.') + 3])
-        rawNumbers = rawNumbers[rawNumbers.index('.') + 3:]
-    for i in range(0, len(yearsTTM)):
-        if yearsTTM[i] == 1 and len(individualValues) > 0:
-            low[i] = individualValues.pop(len(individualValues) - 1)
-            high[i] = individualValues.pop(len(individualValues) - 1)
-    return high, low, True
-
-def yearProcessing(row, rowIndex):
+def yearProcessing(rowIndex):
     errorNum = 14
     markers = [0] * 18
     startVal = 2023
@@ -132,10 +128,10 @@ def yearProcessing(row, rowIndex):
             markers[i] = str(startVal) in row[rowIndex].value
             startVal -= 1
     if markers.count(1) == 0: 
-        return markers, errorHandler(row, errorNum, currSheet)
+        return markers, errorHandler(errorNum, currSheet)
     return markers, True
 
-def redFlagsS1(row, sheetName):
+def redFlagsS1(sheetName):
     #41
     #PE, MKTCAP, Share Short, %Insider, %Institution, 52HighLow, DailyTrade - Series 1
     performanceValues = [5, 8, 10, 11, 12, 15, 16] 
@@ -145,42 +141,42 @@ def redFlagsS1(row, sheetName):
     for i in performanceValues:
         performanceLength.append(len(removeNonNumeric(row[i].value, additionalSet)))
         if i == 5 and len(removeNonNumeric(row[i].value, additionalSet)) == 0: #Nothing is there PE
-            return errorHandler(row, errorNum, sheetName)
+            return errorHandler(errorNum, sheetName)
         if i == 5 and float(removeNonNumeric(row[i].value, additionalSet)) < -100: #PE Over 300
-            return errorHandler(row, errorNum + 1, sheetName)
+            return errorHandler(errorNum + 1, sheetName)
         if i == 5 and float(removeNonNumeric(row[i].value, additionalSet)) > 300: #PE Under -100
-            return errorHandler(row, errorNum + 2, sheetName)
+            return errorHandler(errorNum + 2, sheetName)
         if i == 8 and len(removeNonNumeric(row[i].value, additionalSet)) == 0: #Nothing is there MKTCAP
-            return errorHandler(row, errorNum + 3, sheetName)
+            return errorHandler(errorNum + 3, sheetName)
         if i == 10 and len(removeNonNumeric(row[i].value, additionalSet)) == 0: #Nothing is there SHARE SHORT
-            return errorHandler(row, errorNum + 4, sheetName)
+            return errorHandler(errorNum + 4, sheetName)
         if i == 10 and float(removeNonNumeric(row[i].value, additionalSet)) > 20.0: #Short percentage is over 20 percent...
-            return errorHandler(row, errorNum + 5, sheetName)
+            return errorHandler(errorNum + 5, sheetName)
         if i == 11 and len(removeNonNumeric(row[i].value, additionalSet)) == 0: #Nothing is there Percent Insider
-            return errorHandler(row, errorNum + 6, sheetName)
+            return errorHandler(errorNum + 6, sheetName)
         if i == 12 and float(removeNonNumeric(row[i].value, additionalSet)) == 0: #Nothing is there Institution %
-            return errorHandler(row, errorNum + 7, sheetName)
+            return errorHandler(errorNum + 7, sheetName)
         if i == 15 and len(removeNonNumeric(row[i].value, additionalSet)) == 0: #Nothing is there high low
-            return errorHandler(row, errorNum + 8, sheetName)
+            return errorHandler(errorNum + 8, sheetName)
         if i == 16 and len(removeNonNumeric(row[i].value, additionalSet)) == 0: #Nothing is there daily trade volume
-            return errorHandler(row, errorNum + 9, sheetName)
+            return errorHandler(errorNum + 9, sheetName)
         if i == 16 and performanceLength.count(0) >= 6: # 6 or more fields missing in Series
-            return errorHandler(row, errorNum + 10, sheetName)
+            return errorHandler(errorNum + 10, sheetName)
     return True
 
 #assets less than 1000 eliminated maybe change this
-def redFlagsS3(row, sheetName):
+def redFlagsS3(sheetName):
     performanceValues = [34, 35, 40] 
     additionalSet = ['-', '.'] 
     #Total Liabilities, Total Assets and Number of Employees
     errorNum = 11
     for i in performanceValues: 
         if i == 35 and len(removeNonNumeric(row[i].value, additionalSet)) == 0: #Nothing is there Total Liabilities
-            return errorHandler(row, errorNum, sheetName)
+            return errorHandler(errorNum, sheetName)
         if i == 36 and len(removeNonNumeric(row[i].value, additionalSet)) == 0: #Nothing is there Total Assets
-            return errorHandler(row, errorNum + 1, sheetName)
+            return errorHandler(errorNum + 1, sheetName)
         # if i == 36 and float(removeNonNumeric(row[i].value, additionalSet)) < 10: #Assets less than $10 Million
-        #     return errorHandler(row, errorNum + 2, sheetName)
+        #     return errorHandler(errorNum + 2, sheetName)
     return True
 
 def removeNonNumeric(input, additionalSet):
@@ -196,7 +192,7 @@ def removeNonNumeric(input, additionalSet):
     if counter == 0: return ""
     return output
 
-def errorHandler(row, flag, industry):
+def errorHandler(flag, industry):
     tempWKST = core1['ELIMINATED']
     sheetIndex = sheetNames.index('ELIMINATED')
     flagComment = getErrorCode(flag)
@@ -290,6 +286,10 @@ def getErrorCode(input):
         13: "E13: Total assets are below $1000",
         14: "E14: Years/TTM Missing",
         15: "E15: Yearly High/Low Values Missing",
+        16: "E16: RPS Values Missing",
+        17: "E17: EPS Values Missing",
+        18: "E18: P/E Values Missing",
+        19: "E19: Dividend Yield Value Missing",
         34: "E34: Missing 6 or more of Series 2 fields",
         35: "E35: Series 3 Value is missing",
         36: "E36: ROIC value is below (last 3, last 5 avg and all avg)",
